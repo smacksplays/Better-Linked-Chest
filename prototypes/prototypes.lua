dd0a386dd5fa4ba0be6838d4f1c40e41c7df207b
smacksplays <p.enzinger5555@gmail.com>
2023-06-02 17:44:58 +0200

    locale fix

--- prototypes/prototypes.lua
+++ prototypes/prototypes.lua
@@ -3,9 +3,4 @@
 -- Better Linked Chest
 local func_create_chest = require("better-linked-chest")
 
---require("item.better-linked-chest-item")
---require("recipe.better-linked-chest-recipe")
---require("entity.better-linked-chest-entity")
---require("technology.better-linked-chest-technology")
-
 data:extend{func_create_chest("better-linked-chest", "iron-chest", "container")}
